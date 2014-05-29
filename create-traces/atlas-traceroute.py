aa7ad31067352f586092a83057fef091b58c8a98
Sebastian Castro <sebastian@nzrs.net.nz>
2014-05-29 14:53:10 +1200

    Create all measurements in one request, and improve some of the code to look more like Python. Test case is a little bit longer.

--- create-traces/atlas-traceroute.py
+++ create-traces/atlas-traceroute.py
@@ -14,8 +14,7 @@ class CredentialsNotFound(Exception):
 
 authfile = "%s/.atlas/auth" % os.environ['HOME'];
 
-data = { "definitions": [
-           {   "target": "www.nzrs.net.nz",
+target_def = { "target": "www.nzrs.net.nz",
                "description": "nz test traceroute",
                "type": "traceroute",
                "protocol": "ICMP",
@@ -24,13 +23,14 @@ data = { "definitions": [
                "can_visualize": False,
                "is_public": False,
                "interval": 900
-           } ],
-         "probes": [
-             { "requested": 1,
-                "type": "probes",
-                "value": "0"
-             } ]
-        }
+           }
+probe_def = { "requested": 1,
+              "type": "probes",
+              "value": "0"
+            }
+
+msm = { "definitions": [],
+        "probes": [] }
 
 if not os.path.exists(authfile):
     raise CredentialsNotFound(authfile)
@@ -51,23 +51,29 @@ msm_conf = yaml.safe_load( file(conf_file, 'r') )
 
 for dst in msm_conf['destinations']:
     # Prepare the measurement
-    msm = data
-    msm['definitions'][0]['target'] = dst
+#    target = target_def
+#    target['target'] = dst
+    msm['definitions'].append( dict(target_def, target=dst) )
 
-    request = urllib2.Request(url)
-    request.add_header("Content-Type", "application/json")
-    request.add_header("Accept", "application/json")
-    for src in msm_conf['sources']:
-        try:
-            msm['probes'][0]['value'] = src
-            json_data = json.dumps(msm)
-            conn = urllib2.urlopen(request, json_data)
-            results = json.load(conn) 
-            print("{0} -> {1} : {2:d}".format(src, dst, results["measurements"]))
-            conn.close()
-            time.sleep(2) # Not really necessary
-        except urllib2.HTTPError as e:
-            print >>sys.stderr, ("Fatal error: %s" % e.read())
-            raise
-        conn.close()
+for src in msm_conf['sources']:
+#    probe = probe_def
+#    probe['value'] = src
+    msm['probes'].append( dict(probe_def, value=src, requested=len(msm_conf['sources'])) )
+
+
+request = urllib2.Request(url)
+request.add_header("Content-Type", "application/json")
+request.add_header("Accept", "application/json")
+
+try:
+    json_data = json.dumps(msm)
+    conn = urllib2.urlopen(request, json_data)
+    results = json.load(conn) 
+    print("Created {0}".format(results["measurements"]))
+    conn.close()
+    time.sleep(2) # Not really necessary
+except urllib2.HTTPError as e:
+    print >>sys.stderr, ("Fatal error: %s" % e.read())
+    raise
+conn.close()
 
