d2a6ced665dead37d9bf1aaafa5cf8b5af620ec8
Sebastian Castro <sebastian@nzrs.net.nz>
2014-06-20 16:46:50 +1200

    Messing around with bs, nothing too serious.

--- nzix-bgp-tables/parse-bgp-html.py
+++ nzix-bgp-tables/parse-bgp-html.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 from bs4 import BeautifulSoup
+import re
 
 with open('r1.ape.html') as bgpdata:
-    html = BeautifulSoup(bgpdata)
+    html = BeautifulSoup(bgpdata, 'html.parser')
 
-    print html.title
+    for code in html.body.find_all('code'):
+        for a in code.find_all('a', attrs={'href': re.compile('lg.cgi')}):
+            print a.string
