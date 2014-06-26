b30d0b6d2aedbc9c23b51bc5a04e35b934ae3d00
Sebastian Castro <sebastian@nzrs.net.nz>
2014-06-26 13:47:45 +1200

    Using a list of NZ ASNs and a list of prefixes and paths from routeviews, extract the AS-Paths relevant to NZ ASNs

---
+++ nz-trace-destinations/find-nz-as-path.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import csv
+from collections import OrderedDict
+import json
+
+
+as_list = {}
+
+with open('nz-as-from-rir.tsv', 'rb') as nz_as:
+    as_in = csv.reader(nz_as, delimiter='\t')
+
+    for asn in as_in:
+        as_list[asn[0]] = True
+
+aspath_set = set()
+with open('../rv-bgp-tables/prefix-aspath.txt', 'rb') as aspath_file:
+    aspath_list = csv.reader(aspath_file, delimiter='|')
+
+    for aspath in aspath_list:
+        origin = aspath[1].split(' ')[-1]
+        if as_list.has_key(origin):
+            aspath_set.add(aspath[1])
+
+nz_aspath = []
+for aspath in aspath_set:
+    nz_aspath.append( list(OrderedDict.fromkeys(aspath.split(' '))))
+
+with open('rv-nz-aspath.json', 'wb') as nz_aspath_file:
+    json.dump(dict(aspath=nz_aspath), nz_aspath_file)
+
+
