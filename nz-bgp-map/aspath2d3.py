4a63560bc4361f38e0cb25650c3a7435dfa3e553
Sebastian Castro <sebastian@nzrs.net.nz>
2014-06-26 21:10:28 +1200

    Add the paths discovered from the RouteViews data, and adding a radius property to use within the visualization.

--- nz-bgp-map/aspath2d3.py
+++ nz-bgp-map/aspath2d3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import json
 import networkx as nx
 from networkx.readwrite import json_graph
@@ -29,6 +30,14 @@ for ix in ix_set:
     G.node[ ix ]['name'] = ix.upper()
     G.node[ ix ]['stroke'] = 3
 
+# Add the paths extracted from RV
+with open('../nz-trace-destinations/rv-nz-aspath.json', 'rb') as rv_file:
+    rv_paths = json.load(rv_file)
+
+for aspath in rv_paths['aspath']:
+    if len(aspath) > 1:
+        G.add_edges_from( [ [ aspath[i-1], aspath[i] ] for i in range(2, len(aspath)) ])
+
 # Add labelling based on the country
 with open('../nz-trace-destinations/as-from-rir.tsv', 'rb') as as_info_file:
     as_info = csv.reader(as_info_file, delimiter='\t')
@@ -45,9 +54,18 @@ with open('../nz-trace-destinations/as-from-rir.tsv', 'rb') as as_info_file:
                 G.node[ asn[0] ]['group'] = 'unk'
                 G.node[ asn[0] ]['stroke'] = 3
 
+degree_set = set()
 # Go over the list of nodes and add the degree attribute
 for node_deg in G.degree_iter():
     G.node[ node_deg[0] ]['degree'] = node_deg[1]
+    G.node[ node_deg[0] ]['radius'] = 8 + 10*math.log(node_deg[1],10)
+    degree_set.add( node_deg[1] )
 
 json_dump = json_graph.node_link_data(G)
 json.dump(json_dump, open('nz-bgp-map.json', 'w'))
+
+with open('degree.txt', 'wb') as degree_file:
+    degree_out = csv.writer(degree_file, delimiter='\t')
+
+    for degree_value in degree_set:
+        degree_out.writerow( [degree_value])
