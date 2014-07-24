dd2ffa8d75e466ccdb0c710f086522558065aeb8
Sebastian Castro <sebastian@nzrs.net.nz>
2014-07-24 16:51:23 +1200

    Some code refactoring. Relation symbol accounts now 3 different values on the relation parameter

--- gao-path-analysis.py
+++ gao-path-analysis.py
@@ -10,21 +10,37 @@ def rel_char( src, dst ):
     key = "{0}+{1}".format(src, dst)
     key_rev = "{0}+{1}".format(dst, src)
     if as_rel.has_key(key):
-        rv = '-' if (as_rel[key] == 0) else '>'
+        if (as_rel[key] == 0):
+            rv = '-'
+        elif (as_rel[key] < 0):
+            rv = '>'
+        else:
+            rv = '<'
     elif as_rel.has_key(key_rev):
-        rv = '-' if (as_rel[key_rev] == 0) else '<'
+        if (as_rel[key_rev] == 0):
+            rv = '-'
+        elif (as_rel[key_rev] < 0):
+            rv = '<'
+        else:
+            rv = '>'
 
     return rv
+
+def load_as_relationship(file_list):
+    as_rel = {}
+    for filename in file_list:
+        with open(filename, 'r') as as_rel_file:
+            as_rel_csv = csv.reader(filter(lambda row: row[0]!='#',as_rel_file), delimiter="|")
+            for as_rel_entry in as_rel_csv:
+                [ prov_as, cust_as, rel ] = as_rel_entry
+                as_rel["{0}+{1}".format(prov_as,cust_as)] = int(rel)
+
+    return as_rel
 
 with open('data/rv-nz-aspath.json', 'rb') as rv_file:
     rv_paths = json.load(rv_file)
 
-with open('as-rank/20140601.as-rel.txt', 'rb') as as_rel_file:
-    as_rel_csv = csv.reader(filter(lambda row: row[0]!='#',as_rel_file), delimiter="|")
-    for as_rel_entry in as_rel_csv:
-        [ prov_as, cust_as, rel ] = as_rel_entry
-        as_rel["{0}+{1}".format(prov_as,cust_as)] = int(rel)
-
+as_rel = load_as_relationship(['as-rank/20140601.as-rel.txt', 'data/local-as-rel-info.csv'])
 
 # print "{} {} {}".format('9500', rel_char('9500', '9901'), '9901')
 
@@ -39,7 +55,7 @@ for aspath in rv_paths['aspath']:
             if rel == '?':
                 unknown.append( "|".join([aspath[i-1], aspath[i], rel]))
         if (len(deltas) > 0):
-#            print " ".join(deltas)
+            print " ".join(deltas)
             pass
         else:
             print "WTF with {}".format(aspath)
